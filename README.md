7f85037ab082445c0035ba78d176f0aeb97955d7
Jakub Pawlina <jakub.pawlina@outlook.com>
2020-11-21 23:46:01 +0100

    Interpolation Search algorithm implemented

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Currently implemented algorithms:
 - [Fast Power Modulo](fast_power_modulo.cpp)
 - [Fibonacci Numbers using Matrix](fibonacci_on_matrix.cpp)
 - [Graph components number counting (using DFS)](graph_components_number_DFS.cpp)
+- [Interpolation Search](interpolation_search.cpp)
 - [Knuth-Morris-Pratt (KMP)](kmp.cpp)
 - [Longest Common Subsequence (LCS)](longest_common_subsequence.cpp)
 - [Multiplying Matrixes](multiplying_matrixes.cpp)
---
+++ interpolation_search.cpp
@@ -0,0 +1,52 @@
+// Author: Jakub Pawlina
+// Algorithm : Interpolation Search
+
+#include <bits/stdc++.h>
+
+int interpolation_search(std::vector <int> data, int key)
+{
+	int lower = 0, higher = data.size() - 1;
+	while (lower <= higher && data[lower] <= key && key <= data[higher])
+	{
+		if (lower == higher)
+		{
+			if (data[lower] == key) return lower;
+			return -1;
+		}
+
+		double fraction = (double) ((higher - lower) / (data[higher] - data[lower]));
+		int position = lower + (fraction * (key - data[lower]));
+
+		if (data[position] == key) return position;
+		if (data[position] < key) lower = position + 1;
+		if (data[position] > key) higher = position - 1;
+	}
+	return -1;
+}
+
+int32_t main()
+{
+	int elements_number;
+	std::cin >> elements_number;
+
+	std::vector <int> data;
+
+	for (int i = 0; i < elements_number; ++i)
+	{
+		int tmp; std::cin >> tmp;
+		data.push_back(tmp);
+	}
+
+	int key_number;
+	std::cin >> key_number;
+
+	for (int i = 0; i < key_number; ++i)
+	{
+		int key; std::cin >> key;
+		int position = interpolation_search(data, key);
+		if (!~position) std::cout << "- ";
+		else std::cout << position + 1 << ' ';
+	}
+
+	return 0;
+}
